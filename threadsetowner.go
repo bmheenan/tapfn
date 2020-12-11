a52722fc5bd7484962e1a949fb75d32202dc7384
bmheenan <bmheenan@users.noreply.github.com>
2020-12-11 09:25:08 -0500

    Fix ThreadSetOwner: adds new owner as a stakeholder

--- threadsetowner.go
+++ threadsetowner.go
@@ -7,10 +7,12 @@ func (cn *cnTapdb) ThreadSetOwner(id int64, owner string) {
 	if err != nil {
 		panic(fmt.Sprintf("No thread with id %v", id))
 	}
+	po := th.Owner
 	for _, d := range cn.db.GetThreadDes(id) {
-		if d.Owner != th.Owner {
+		if d.Owner != po {
 			continue
 		}
+		cn.ThreadAddStk(d.ID, owner)
 		cn.db.SetOwner(d.ID, owner)
 		cn.recalcAllStkCosts(d.ID)
 	}
